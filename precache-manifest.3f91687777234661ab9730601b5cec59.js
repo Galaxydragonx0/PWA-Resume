0c679b8fa66d993e545523156a24b60ad01073c5
Galaxydragonx0 <48951583+Galaxydragonx0@users.noreply.github.com>
2020-09-10 14:24:37 -0400

    Updates

---
+++ precache-manifest.3f91687777234661ab9730601b5cec59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "334d07960f59ca04b815546c81223016",
+    "url": "/PWA-Resume/index.html"
+  },
+  {
+    "revision": "a9a36ba56b0aea9c5fc7",
+    "url": "/PWA-Resume/static/css/main.3f8afc2b.chunk.css"
+  },
+  {
+    "revision": "691fe4b388a83104ec40",
+    "url": "/PWA-Resume/static/js/2.39ae37d4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/PWA-Resume/static/js/2.39ae37d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9a36ba56b0aea9c5fc7",
+    "url": "/PWA-Resume/static/js/main.f6936dc8.chunk.js"
+  },
+  {
+    "revision": "6ad0f76130a3b08042c1",
+    "url": "/PWA-Resume/static/js/runtime-main.9c889aac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PWA-Resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
